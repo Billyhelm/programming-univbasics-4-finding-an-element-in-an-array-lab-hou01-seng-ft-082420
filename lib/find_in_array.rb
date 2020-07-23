45baae70484f527e8eb37024dcee565b75b3bdb7
Billy Helm <billyhelm88@gmail.com>
2020-07-23 14:57:48 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   i=0 
   while i<array.length do
-    array[i] == value_to_find ? (i, break) : i += 1 
+    array[i] == value_to_find ? i : i += 1 
   end 
 end
